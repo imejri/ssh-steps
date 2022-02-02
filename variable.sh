3e2e222cf6921cc3d6d5c742e353bf14ae919d82
MEJRI ISSAM <34856078+imejri@users.noreply.github.com>
2022-02-02 19:50:36 +0100

    Update variable.sh

--- variable.sh
+++ variable.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export CI=true
-VERSION=1.0-SNAPSHOT
+VERSION="1.0-SNAPSHOT"
 DEST_DIR=gaston
 ARTIFACTORY_KEY=$2
 USER=$1
-jfrog rt dl fleetops-local/simple-project-$VERSION.jar --url http://64.225.51.239:8082/artifactory/ --user '$USER' --password '$ARTIFACTORY_KEY'
+jfrog rt dl fleetops-local/simple-project-'$VERSION'.jar --url http://64.225.51.239:8082/artifactory/ --user '$USER' --password '$ARTIFACTORY_KEY'
